931a1964184b5b4a2f2e4b22d9415e33290997fa
Elias Saric <eliassaric@gmail.com>
2015-02-25 21:54:24 -0500

    outline complete

--- 02KnightsTour/KnightsTour.java
+++ 02KnightsTour/KnightsTour.java
@@ -1,3 +1,48 @@
 public class KnightsTour {
 	
+	public int[][] board;
+	public int size;
+	
+	public static void main(String[]args) {
+		KnightsTour knight = new KnightsTour(5);
+		System.out.println(knight);
+	}
+	
+	public KnightsTour(int size) {
+		board = new int[size][size];
+		this.size = size;
+	}
+	
+	public String name() {
+		return "saric.elias";
+	}
+	
+	public String toString() {
+		String outString = "";
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (board[i][j] < 100) {
+					outString += " ";
+				}
+				if (board[i][j] < 10) {
+					outString += " ";
+				}
+				outString += board[i][j];
+			}
+			outString += "\n";
+		}
+		return outString;
+	}
+	
+	public boolean solve() {
+		solve(0,0);
+	}
+	
+	public boolean solve(int x, int y) {
+		solve(x, y, 0);
+	}
+	
+	public boolean solve(int x, int y, int currentMove) {
+		return false;
+	}
 }
