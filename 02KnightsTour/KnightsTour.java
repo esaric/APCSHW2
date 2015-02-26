3740d4011d5adac546ca61d04e6fd91fc26097e6
Elias Saric <eliassaric@gmail.com>
2015-02-25 22:49:12 -0500

    almost

--- 02KnightsTour/KnightsTour.java
+++ 02KnightsTour/KnightsTour.java
@@ -1,17 +1,33 @@
 public class KnightsTour {
 	
-	public int[][] board;
-	public int size;
+	private int[][] board;
+	private int size;
 	
-	public static void main(String[]args) {
+	final static String clear =  "\033[2J";
+	final static String hide =  "\033[?25l";
+	final static String show =  "\033[?25h";
+	
+	/*public static void main(String[]args) {
 		KnightsTour knight = new KnightsTour(5);
-		System.out.println(knight);
-	}
+		knight.solve();
+	}*/
 	
 	public KnightsTour(int size) {
 		board = new int[size][size];
 		this.size = size;
 	}
+	
+	private String go(int x,int y){
+		return ("\033[" + x + ";" + y + "H");
+    }
+ 
+    public void wait(int millis){
+		try {
+			Thread.sleep(millis);
+		}
+		catch (InterruptedException e) {
+		}
+    }
 	
 	public String name() {
 		return "saric.elias";
@@ -35,14 +51,36 @@ public class KnightsTour {
 	}
 	
 	public boolean solve() {
-		solve(0,0);
+		return solve(0,0);
 	}
 	
 	public boolean solve(int x, int y) {
-		solve(x, y, 0);
+		return solve(x, y, 1);
 	}
 	
 	public boolean solve(int x, int y, int currentMove) {
+		
+		/*System.out.println(this);
+		wait(20);*/
+		
+		if (currentMove > size * size) {
+			return true;
+		}
+		if (x >= size || x < 0 || y >= size || y < 0) {
+			return false;
+		}
+		if (board[x][y] == 0) {
+			board[x][y] = currentMove;
+			return solve(x + 2, y + 1, currentMove + 1) ||
+				   solve(x + 2, y - 1, currentMove + 1) ||
+				   solve(x + 1, y + 2, currentMove + 1) ||
+				   solve(x + 1, y - 2, currentMove + 1) ||
+				   solve(x - 1, y + 2, currentMove + 1) ||
+				   solve(x - 1, y - 2, currentMove + 1) ||
+				   solve(x - 2, y + 1, currentMove + 1) ||
+				   solve(x - 2, y - 1, currentMove + 1);
+		}
+		board[x][y] = 0;
 		return false;
 	}
 }
