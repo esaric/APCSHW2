93da6384168882cb341f4874484c0334c277829d
Elias Saric <eliassaric@gmail.com>
2015-04-19 15:05:00 -0400

    Priority Queue added, Best and AStar searches fully functional

--- 11Maze/Maze.java
+++ 11Maze/Maze.java
@@ -5,13 +5,14 @@ public class Maze {
     private static final String hide = "\033[?25l";
     private static final String show = "\033[?25h";
     private String go(int x,int y){
-	return ("\033[" + x + ";" + y + "H");
+		return ("\033[" + x + ";" + y + "H");
     }
     private static final int DFS = 0;
     private static final int BFS = 1;
     private static final int BEST = 2;
     private static final int ASTAR = 3;
     private char[][] array;
+	
     public Maze() {}
     public Maze(String filename) {
 	try {
@@ -44,7 +45,7 @@ public class Maze {
 	for (int i = 0; i < array.length; i++) {
 	    for (int j = 0; j < array[0].length; j++) {
 		if (array[i][j] == 'S') {
-		    return new Coordinate(j, i, null);
+		    return new Coordinate(j, i);
 		}
 	    }
 	}
@@ -54,118 +55,152 @@ public class Maze {
 	for (int i = 0; i < array.length; i++) {
 	    for (int j = 0; j < array[0].length; j++) {
 		if (array[i][j] == 'E') {
-		    return new Coordinate(j, i, null);
+		    return new Coordinate(j, i);
 		}
 	    }
 	}
 	return null;
     }
     public boolean solve(boolean animate, int mode) {
-	Frontier front = new Frontier(mode);
-	front.enqueue(findStart());
-	while (!front.isEmpty()) {
-	    //System.out.println(front);
-	    Coordinate removed = front.dequeue();
-	    //System.out.println(front);
-	    //System.out.println(removed);
-	    if (array[removed.getY()][removed.getX()] == 'E') {
-		while (removed != null) {
-		    array[removed.getY()][removed.getX()] = '@';
-		    removed = removed.getPrevious();
-		    if (animate) {
-			try {
-			    Thread.sleep(50);
-			} catch(InterruptedException ex) {
-			    Thread.currentThread().interrupt();
+		Frontier front = new Frontier(mode);
+		front.enqueue(findStart());
+		while (!front.isEmpty()) {
+			//System.out.println(front);
+			Coordinate removed = front.dequeue();
+			//System.out.println(front);
+			//System.out.println(removed);
+			if (array[removed.getY()][removed.getX()] == 'E') {
+			while (removed != null) {
+				array[removed.getY()][removed.getX()] = '@';
+				removed = removed.getPrevious();
+				if (animate) {
+					try {
+						Thread.sleep(50);
+					} catch(InterruptedException ex) {
+						Thread.currentThread().interrupt();
+					}
+					System.out.println(clear + hide + go(0,0) + this + show);
+				}
 			}
-			System.out.println(clear + hide + go(0,0) + this + show);
-		    }
+			return true;
+			}
+			array[removed.getY()][removed.getX()] = 'X';
+			//System.out.println(front);
+			if (array[removed.getY()+1][removed.getX()] != '#' && array[removed.getY()+1][removed.getX()] != 'X') {
+				front.enqueue(new Coordinate(removed.getX(), removed.getY()+1, removed));
+			}
+			//System.out.println(front);
+			if (array[removed.getY()-1][removed.getX()] != '#' && array[removed.getY()-1][removed.getX()] != 'X') {
+				front.enqueue(new Coordinate(removed.getX(), removed.getY()-1, removed));
+			}
+			if (array[removed.getY()][removed.getX()+1] != '#' && array[removed.getY()][removed.getX()+1] != 'X') {
+				front.enqueue(new Coordinate(removed.getX()+1, removed.getY(), removed));
+			}
+			if (array[removed.getY()][removed.getX()-1] != '#' && array[removed.getY()][removed.getX()-1] != 'X') {
+				front.enqueue(new Coordinate(removed.getX()-1, removed.getY(), removed));
+			}
+			if (animate) {
+				try {
+					Thread.sleep(50);
+				} catch(InterruptedException ex) {
+					Thread.currentThread().interrupt();
+				}
+				System.out.println(clear + hide + go(0,0) + this + "\n" + front + show);
+			}
+			//System.out.println("----------------------");
 		}
-		return true;
-	    }
-	    array[removed.getY()][removed.getX()] = 'X';
-	    //System.out.println(front);
-	    if (array[removed.getY()+1][removed.getX()] != '#' && array[removed.getY()+1][removed.getX()] != 'X') {
-		front.enqueue(new Coordinate(removed.getX(), removed.getY()+1, removed));
-	    }
-	    //System.out.println(front);
-	    if (array[removed.getY()-1][removed.getX()] != '#' && array[removed.getY()-1][removed.getX()] != 'X') {
-		front.enqueue(new Coordinate(removed.getX(), removed.getY()-1, removed));
-	    }
-	    if (array[removed.getY()][removed.getX()+1] != '#' && array[removed.getY()][removed.getX()+1] != 'X') {
-		front.enqueue(new Coordinate(removed.getX()+1, removed.getY(), removed));
-	    }
-	    if (array[removed.getY()][removed.getX()-1] != '#' && array[removed.getY()][removed.getX()-1] != 'X') {
-		front.enqueue(new Coordinate(removed.getX()-1, removed.getY(), removed));
-	    }
-	    if (animate) {
-		try {
-		    Thread.sleep(50);
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
-		}
-		System.out.println(clear + hide + go(0,0) + this + "\n" + front + show);
-	    }
-	    //System.out.println("----------------------");
-	}
-	return false;
+		return false;
     }
     public boolean solveBFS(boolean animate) {
-	return solve(animate, BFS);
+		return solve(animate, BFS);
     }
     public boolean solveDFS(boolean animate) {
-	return solve(animate, DFS);
+		return solve(animate, DFS);
     }
+	public boolean solveBest(boolean animate) {
+		return solve(animate, BEST);
+	}
+	public boolean solveAStar(boolean animate) {
+		return solve(animate, ASTAR);
+	}
     public boolean solveBFS() {return solveBFS(false);}
     public boolean solveDFS() {return solveDFS(false);}
+	public boolean solveBest() {return solveBest(false);}
+	public boolean solveAStar() {return solveAStar(false);}
+	
+	private int distanceToEnd(Coordinate cor) {
+		Coordinate end = findEnd();
+		return Math.abs(cor.getX() - end.getX()) + Math.abs(cor.getY() - end.getY());
+	}
+	
     private class Coordinate {
-	private int x;
-	private int y;
-	private Coordinate previous;
-	public Coordinate(int x, int y, Coordinate previous) {
-	    this.x = x;
-	    this.y = y;
-	    this.previous = previous;
-	}
-	public String toString() {
-	    return "(" + y + ", " + x + ")";
-	}
-	public int getX() {return x;}
-	public int getY() {return y;}
-	public Coordinate getPrevious() {return previous;}
+		private int x;
+		private int y;
+		private int steps;
+		private Coordinate previous;
+		public Coordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+			previous = null;
+			steps = 0;
+		}
+		public Coordinate(int x, int y, Coordinate previous) {
+			this.x = x;
+			this.y = y;
+			this.previous = previous;
+			steps = previous.getSteps() + 1;
+		}
+		public String toString() {
+			return "(" + y + ", " + x + ")";
+		}
+		public int getX() {return x;}
+		public int getY() {return y;}
+		public int getSteps() {return steps;}
+		public Coordinate getPrevious() {return previous;}
     }
     private class Frontier{
-	private MyDeque<Coordinate> queue = new MyDeque<Coordinate>();
-	private int mode;
-	public Frontier(int mode) {
-	    this.mode = mode;
-	}
-	public String toString() {
-	    System.out.println(queue);
-	    return "";
-	}
-	public Coordinate enqueue(Coordinate cor) {
-	    queue.addFirst(cor);
-	    return cor;
-	}
-	public Coordinate dequeue() {
-	    if (mode == 0) {
-		return queue.removeFirst();
-	    }else if (mode == 1) {
-		return queue.removeLast();
-	    }else {
-		return null;
-	    }
-	}
-	public Coordinate peek() {
-	    return queue.getFirst();
-	}
-	public boolean isEmpty() {
-	    return queue.getLength() == 0;
-	}
+		private MyDeque<Coordinate> queue = new MyDeque<Coordinate>();
+		private int mode;
+		public Frontier(int mode) {
+			this.mode = mode;
+		}
+		public String toString() {
+			System.out.println(queue);
+			return "";
+		}
+		public Coordinate enqueue(Coordinate cor) {
+			if (mode == 0 || mode == 1) {
+				queue.addFirst(cor);
+			}
+			if (mode == 2) {
+				queue.add(cor, distanceToEnd(cor));
+			}
+			if (mode == 3) {
+				queue.add(cor, distanceToEnd(cor) + cor.getSteps());
+			}
+			return cor;
+		}
+		public Coordinate dequeue() {
+			if (mode == 0) {
+				return queue.removeFirst();
+			}
+			if (mode == 1) {
+				return queue.removeLast();
+			}
+			if (mode == 2 || mode == 3) {
+				return queue.removeSmallest();
+			}
+			return null;
+		}
+		public Coordinate peek() {
+			return queue.getFirst();
+		}
+		public boolean isEmpty() {
+			return queue.getLength() == 0;
+		}
     }
     public static void main(String[]args) {
-	Maze maze = new Maze("maze.txt");
-	maze.solveBFS(true);
+		Maze maze = new Maze("data3.dat");
+		maze.solveBest(true);
     }
 }
--- 11Maze/MyDeque.java
+++ 11Maze/MyDeque.java
@@ -1,62 +1,71 @@
 import java.util.*;
 public class MyDeque<T> {
     private Object[] array;
+	private int[] weights;
     private int head;
     private int tail;
     private int size;
+	
     public MyDeque() {
-	array = new Object[4];
-	head = 0;
-	tail = 0;
-	size = 0;
+		array = new Object[4];
+		weights = new int[4];
+		head = 0;
+		tail = 0;
+		size = 0;
     }
     public String toString() {
-	String output = "";
-	int index = 0;
-	while (index < size) {
-	    output += array[(head+index) % array.length] + " ";
-	    index++;
-	}
-	//return "[ " + output + "]";
-	return "[ " + output + "] " + "Tail: " + tail + " Head: " + head + "\n" + Arrays.toString(array);
+		String output = "[ ";
+		int index = 0;
+		while (index < size) {
+			output += array[(head+index) % array.length] + " ";
+			index++;
+		}
+		output += "]\n[ ";
+		index = 0;
+		while (index < size) {
+			output += weights[(head+index) % array.length] + " ";
+			index++;
+		}
+		return output + "]";
+		//return "[ " + output + "] " + "Tail: " + tail + " Head: " + head + "\n" + Arrays.toString(array);
     }
     public T getFirst() {
-	return (T)array[head];
+		return (T)array[head];
     }
     public T getLast() {
-	return (T)array[tail];
+		return (T)array[tail];
     }
     public T removeFirst() {
-	if (size == 0) {
-	    throw new NoSuchElementException();
-	}
-	T output = (T)array[head];
-	array[head] = null;
-	if (size == 1) {
-	    head = tail;
-	}else if (head == array.length - 1) {
-	    head = 0;
-	}else {
-	    head++;
-	}
-	size--;
-	return output;
+		if (size == 0) {
+			throw new NoSuchElementException();
+		}
+		T output = (T)array[head];
+		array[head] = null;
+		if (size == 1) {
+			head = tail;
+		}else if (head == array.length - 1) {
+			head = 0;
+		}else {
+			head++;
+		}
+		size--;
+		return output;
     }
     public T removeLast() {
-	if (size == 0) {
-	    throw new NoSuchElementException();
-	}
-	T output = (T)array[tail];
-	array[tail] = null;
-	if (size == 1) {
-	    tail = head;
-	}else if (tail == 0) {
-	    tail = array.length - 1;
-	}else {
-	    tail--;
-	}
-	size--;
-	return output;
+		if (size == 0) {
+			throw new NoSuchElementException();
+		}
+		T output = (T)array[tail];
+		array[tail] = null;
+		if (size == 1) {
+			tail = head;
+		}else if (tail == 0) {
+			tail = array.length - 1;
+		}else {
+			tail--;
+		}
+		size--;
+		return output;
     }
     public void addFirst(T value) {
 	if (size == 0) {
@@ -91,26 +100,91 @@ public class MyDeque<T> {
 	size++;
     }
     private void resize() {
-	Object[] newArray = new Object[array.length * 2];
-	int index = 0;
-	while (index < size) {
-	    newArray[index] = array[(head+index) % array.length];
-	    index++;
-	}
-	head = 0;
-	tail = size - 1;
-	array = newArray;
+		Object[] newArray = new Object[array.length * 2];
+		int[] newWeights = new int[weights.length * 2];
+		int index = 0;
+		while (index < size) {
+			newArray[index] = array[(head+index) % array.length];
+			newWeights[index] = weights[(head+index) % weights.length];
+			index++;
+		}
+		head = 0;
+		tail = size - 1;
+		array = newArray;
+		weights = newWeights;
     }
     public int getLength() {
-	return size;
+		return size;
     }
+	public T add(T value, int priority) {
+		if (size == array.length) {
+			resize();
+		}
+		if (size > 0) {
+			tail++;
+		}
+		if (tail == array.length) {
+			tail = 0;
+	    }
+		array[tail] = value;
+		weights[tail] = priority;
+		size++;
+		return value;
+	}
+	public T removeSmallest() {
+		if (size == 0) {
+			throw new NoSuchElementException();
+		}
+		swap(head, findSmallest());
+		T output = (T)array[head];
+		array[head] = null;
+		weights[head] = -1;
+		size--;
+		if (size > 0) {
+			head++;
+		}
+		if (head == array.length) {
+			head = 0;
+		}
+		return output;
+	}
+	private void swap(int first, int last) {
+		if (first > array.length || last > array.length || first < 0 || last < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		Object tempObj = array[last];
+		int tempInt = weights[last];
+		array[last] = array[first];
+		weights[last] = weights[first];
+		array[first] = tempObj;
+		weights[first] = tempInt;
+	}
+	private int findSmallest() {
+		int min = weights[head];
+		int minIndex = head;
+		int index = 0;
+		while (index < size) {
+			int currentIndex = (head+index) % weights.length;
+			if (min > weights[currentIndex]) {
+				min = weights[currentIndex];
+				minIndex = currentIndex;
+			}
+			index++;
+		}
+		return minIndex;
+	}
     public static void main(String[]args) {
-	MyDeque<Integer> d = new MyDeque<Integer>();
-	d.addFirst(5);
-	//d.addFirst(6);
-	d.removeFirst();
-	System.out.println(d);
-	d.addFirst(7);
-	System.out.println(d);
+		MyDeque<String> d = new MyDeque<String>();
+		d.add("1", 1);
+		d.add("2", 2);
+		System.out.println(d);
+		System.out.println(d.removeSmallest());
+		System.out.println(d);
+		d.add("3", 3);
+		System.out.println(d);
+		System.out.println(d.removeSmallest());
+		System.out.println(d);
+		d.add("4", 4);
+		System.out.println(d);
     }
 }
