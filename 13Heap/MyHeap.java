c1533622f2ab31f5c48643ba76d61ff2c690dc37
Elias Saric <eliassaric@gmail.com>
2015-05-14 20:36:22 -0400

    remove still broken :9

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,28 +1,94 @@
+import java.util.*;
+
 public class MyHeap {
-	
-	private int[] heap;
-	private int mode;
-	private static final int MAX = 0;
-	private static final int MIN = 1;
-	
-	public MyHeap() {
-		MyHeap(MAX);
+
+    private int[] heap;
+    private int mode;
+
+    private static final int MAX = 1;
+    private static final int MIN = -1;
+    private static final int UP = 1;
+    private static final int DOWN = -1;
+
+    public MyHeap() {
+	this(MAX);
+    }
+    public MyHeap(int mode) {
+	this.mode = mode;
+	heap = new int[4];
+    }
+
+    public void add(int val) {
+	if (heap[0] + 1 == heap.length) {
+	    resize();
 	}
-	public MyHeap(int mode) {
-		this.mode = mode;
-		heap = new int[2];
+	if (heap[0] == 0) {
+	    heap[1] = val;
+	}else {
+	    heap[heap[0]+1] = val;
+	    shiftUp();
 	}
-	
-	public void add(int val) {}
-	
-	public void int remove(int val) {
-		return val;
+	heap[0]++;
+    }
+    public int remove() throws NoSuchElementException{
+	if (heap[0] == 0) {
+	    throw new NoSuchElementException();
 	}
-	
-	private static boolean compare(int num1, int num2) {
-		if (mode == MAX)
-			return num1 >= num2;
-		else
-			return num2 >= num1;
+	int output = heap[1];
+	swap(1, heap[0]+1);
+	shiftDown();
+	heap[heap[0]+1] = 0;
+	heap[0]--;
+	return output;
+    }
+
+    public String toString() {
+	return Arrays.toString(heap);
+    }
+
+    private void swap(int index1, int index2) {
+	int temp = heap[index1];
+	heap[index1] = heap[index2];
+	heap[index2] = temp;
+    }
+    private void resize() {
+	heap = Arrays.copyOf(heap, (heap[0]+1)*2);
+    }
+    private void shiftUp() {
+	int last = heap[0]+1;
+	while (heap[last]*mode >= heap[last/2]*mode && last != 1) {
+	    //System.out.println(heap[last]);
+	    swap(last, last/2);
+	    last = last/2;
 	}
+    }
+    private void shiftDown() {
+	int first = 1;
+	while (first*2 < heap[0]+1
+	       && (heap[first]*mode < heap[first*2]*mode 
+	       || heap[first]*mode < heap[first*2+1]*mode)) {
+	    if (heap[first*2] > heap[first*2+1]) {
+		swap(first, first*2);
+		first = first * 2;
+	    }else{
+		swap(first, first*2+1);
+		first = first * 2 + 1;
+	    }
+	}
+    }
+
+    public static void main(String[] args) {
+	MyHeap h = new MyHeap();
+	System.out.println(h);
+	h.add(5);
+	h.add(4);
+	h.add(6);
+	h.remove();
+	System.out.println(h);
+	h.remove();
+	System.out.println(h);
+	h.remove();
+	System.out.println(h);
+	h.remove();
+    }
 }
